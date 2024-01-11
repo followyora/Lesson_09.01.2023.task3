40c8f151c7418c744a4232ba0eebe874d2436681
Pasha.Oryshchych <followyora@gmail.com>
2024-01-11 10:19:56 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/*Создайте метод, который принимает переменную типа List. Выведите описание всех методов переданного списка.
+В main вызовите метод, передав в него экземпляр ArrayList, экземпляр LinkedList
+экземпляр, полученный методом List.of().
+
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        printMethodList(new ArrayList<>());
+        System.out.println("---------");
+        printMethodList(new LinkedList<>());
+        System.out.println("---------");
+        printMethodList(List.of());
+    }
+
+    private static void printMethodList(List<String> stringList) {
+        Class<List<String>> clazz = (Class<List<String>>) stringList.getClass();
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            String name = method.getName();
+            String returnType = method.getReturnType().getSimpleName();
+            List<String> inputTypes = Arrays.stream(method.getParameterTypes()).map(Class::getSimpleName).toList();
+            List<String> exceptionTypes = Arrays.stream(method.getExceptionTypes()).map(Class::getSimpleName).toList();
+            System.out.printf("Method %s, Return %s, Params %s, Exceptions %s %n", name, returnType, inputTypes, exceptionTypes);
+
+        }
+    }
+}
